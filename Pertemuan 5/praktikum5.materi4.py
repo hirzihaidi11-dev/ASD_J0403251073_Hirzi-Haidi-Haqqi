61cbef32ac6b354e5a3d85293c7209fc89a9cb1a
hirzihaidi <hirzihaidi11@gmail.com>
2026-02-26 22:05:08 +0700

    menambahkan materi 4 dan 5

---
+++ Pertemuan 5/praktikum5.materi4.py
@@ -0,0 +1,23 @@
+#========IDENTITAS MAHASISWA===================
+#NAMA : Hirzi Haidi Haqqi
+#NIM : J0403251073
+#Kelas : TPL-P2
+#==============================================
+
+# ========================================================== 
+# Contoh Backtracking 1: Kombinasi Biner (n) 
+# ========================================================== 
+
+def biner(n, hasil=""): 
+    # Base case: jika panjang string sudah n, cetak hasil 
+    if len(hasil) == n: 
+        print(hasil) 
+        return
+    
+    # Choose + Explore: tambah '0' 
+    biner(n, hasil + "0") 
+    
+    # Choose + Explore: tambah '1' 
+    biner(n, hasil + "1") 
+    
+biner(3)
